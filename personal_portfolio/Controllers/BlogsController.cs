1e2d26f81362a41a4f935ce496326e91f5a15ce0
Leo Zhang <leotheasianlion@gmail.com>
2023-02-07 10:27:24 -0500

    added sorting functionality to blog posts

--- personal_portfolio/Controllers/BlogsController.cs
+++ personal_portfolio/Controllers/BlogsController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Personal_Portfolio.Models;
 using Personal_Portfolio.Services;
 
@@ -16,20 +17,25 @@ public class BlogsController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    // public override void OnActionExecuted(ActionExecutedContext filterContext)
+    // {
+    //     // Perform any processing that needs to be done after the action has executed
+    //     base.OnActionExecuted(filterContext);
+
+    //     Console.WriteLine("Initted blog index page");
+    // }
+
+    public IActionResult Index(string isNewest)
     {
-        return View(dataRepo.GetData());
+        ViewBag.SortBy = isNewest == "Newest" ? "Oldest" : "Newest";
+        return View(dataRepo.Sort(ViewBag.SortBy
+        == "Newest"));
     }
 
     public IActionResult BlogDetails(string blogTitle)
     {
         var blog = dataRepo.GetData().FirstOrDefault(b => b.title == blogTitle);
         return View(blog);
-    }
-
-    public PartialViewResult BlogsListPartial()
-    {
-        return PartialView("_BlogCardPartialView", dataRepo.GetData());
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
